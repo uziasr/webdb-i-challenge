a8ec515d4834498fcda20a0859836cad5211e865
Uzias Rivera <uziasrivera@gmail.com>
2019-11-04 13:58:20 -0800

    completed queries and crud

---
+++ budgetRouter/budgetRouter.js
@@ -0,0 +1,68 @@
+const express = require('express')
+
+const knex = require('../data/dbConfig')
+const router = express.Router()
+
+router.get('/', (req,res)=>{
+    knex
+    ('accounts')
+    .then(accounts=>{
+        res.json(accounts)
+    })
+    .catch(err=>{
+        res.status(500).json({error:'something went wrong'})
+    })
+})
+
+router.get('/:id', (req,res)=>{
+    knex
+    .select('*')
+    .from('accounts')
+    .where('id', '=', req.params.id)
+    .then(accounts=>{{
+        accounts.length>0? res.status(200).json(accounts): res.status(404).json({error:"that id does not exist"})
+    }})
+    .catch(err=>{
+        res.status(500).json({error: 'something went wrong'})
+    })
+})
+
+
+router.post('/', (req,res)=>{
+    knex('accounts')
+    .insert({name: req.body.name, budget: req.body.budget})
+    .then(accounts=>{
+        res.status(200).json({...req.body})
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error: 'something went wrong'})
+    })
+})
+
+router.put('/:id', (req,res)=>{
+    knex('accounts')
+    .where('id', '=', req.params.id)
+    .update({name: req.body.name, budget: req.body.budget})
+    .then(accounts=>{
+        res.status(200).json({name: req.body.name, budget: req.body.budget})
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error:'something went wrong'})
+    })
+})
+
+router.delete('/:id', (req,res)=>{
+    knex('accounts')
+    .where({id: req.params.id})
+    .del()
+    .then(accounts=>{
+        res.status(200).json({success:"It was successfully deleted "})
+    })
+    .catch(err=>{
+        res.status(500).json({error: "something went wrong"})
+    })
+})
+
+module.exports = router;
